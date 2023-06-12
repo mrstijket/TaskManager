2ff0a403e0542f62d7a23f8338ed8e350d9a656c
MrStijket <mr.stijket@gmail.com>
2023-06-12 11:53:36 +0300

    Setting MongoDB with Mongoose

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config();
+
+// middleware
+
+app.use(express.json());
+
+
+app.get('/hello', (req, res) => {
+  res.send('Hello!');
+});
+
+app.use('/api/v1/tasks', tasks);
+
+const port = 5000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+start();
