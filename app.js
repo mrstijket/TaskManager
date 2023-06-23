5256b4c8c2dd7edf720d0ee6536cbf3f6bebde13
Mr.Stijket <mr.stijket@gmail.com>
2023-06-24 00:34:59 +0300

    Final Version

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
 require('dotenv').config();
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
 // middleware
-
+app.use(express.static('./public'));
 app.use(express.json());
 
-
-app.get('/hello', (req, res) => {
-  res.send('Hello!');
-});
-
+// routes
 app.use('/api/v1/tasks', tasks);
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
---
+++ middleware/async.js
@@ -0,0 +1,11 @@
+const aysncWrapper = (func) => {
+  return async (req, res, next) => {
+    try {
+      await func(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+module.exports = aysncWrapper;
